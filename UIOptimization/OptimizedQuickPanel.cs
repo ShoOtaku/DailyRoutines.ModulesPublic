3bb5058b45c938aeed5893d938dc910d9aa0b72e
AtmoOmen <atmoomen2004@outlook.com>
2025-12-23 22:07:29 +0800

    OptimizedQuickPanel

--- UIOptimization/OptimizedQuickPanel.cs
+++ UIOptimization/OptimizedQuickPanel.cs
@@ -1,6 +1,36 @@
-﻿namespace DailyRoutines.ModulesPublic;
+﻿using DailyRoutines.Abstracts;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using Lumina.Excel.Sheets;
+using Lumina.Text.ReadOnly;
 
-public class OptimizedQuickPanel
+namespace DailyRoutines.ModulesPublic;
+
+public unsafe class OptimizedQuickPanel : DailyModuleBase
 {
+    private static readonly TextCommand QuickPanelLine = LuminaGetter.GetRowOrDefault<TextCommand>(50);
     
+    protected override void Init()
+    {
+        ChatManager.RegPreExecuteCommandInner(OnPreExecuteCommandInner);
+    }
+
+    protected override void Uninit()
+    {
+        ChatManager.Unreg(OnPreExecuteCommandInner);
+    }
+
+    private static void OnPreExecuteCommandInner(ref bool isPrevented, ref ReadOnlySeString message)
+    {
+        var messageText = message.ToString();
+        if (!messageText.StartsWith('/')) return;
+        if (messageText.Split(' ') is not { Length: 2 } prasedCommand                                                      ||
+            (prasedCommand[0] != QuickPanelLine.Command.ToString() && prasedCommand[0] != QuickPanelLine.Alias.ToString()) ||
+            !int.TryParse(prasedCommand[1], out var index)                                                                 ||
+            index - 1 < 0                                                                                                  ||
+            index     > 4)
+            return;
+        
+        AgentQuickPanel.Instance()->OpenPanel((uint)(index - 1), showFirstTimeHelp: false);
+        isPrevented = true;
+    }
 }
