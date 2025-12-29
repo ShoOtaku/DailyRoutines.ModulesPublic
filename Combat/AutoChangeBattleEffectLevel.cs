06af64b1f8bd63fa4d8469d9b361a9361b059ebc
AtmoOmen <atmoomen2004@outlook.com>
2025-12-29 14:49:31 +0800

    AutoChangeBattleEffectLevel

---
+++ Combat/AutoChangeBattleEffectLevel.cs
@@ -0,0 +1,347 @@
+using System;
+using System.Collections.Generic;
+using DailyRoutines.Abstracts;
+using DailyRoutines.Managers;
+using Dalamud.Game.Config;
+using Dalamud.Interface.Components;
+using Dalamud.Plugin.Services;
+using Lumina.Excel.Sheets;
+
+namespace DailyRoutines.ModulesPublic;
+
+public class AutoChangeBattleEffectLevel : DailyModuleBase
+{
+    public override ModuleInfo Info { get; } = new()
+    {
+        Title       = GetLoc("AutoChangeBattleEffectLevelTitle"),
+        Description = GetLoc("AutoChangeBattleEffectLevelDescription"),
+        Category    = ModuleCategories.Combat,
+        Author      = ["Siren"]
+    };
+
+    public override ModulePermission Permission { get; } = new() { AllDefaultEnabled = true };
+
+    private static Config ModuleConfig = null!;
+
+    private static EffectSetting? LastAppliedSettings;
+
+    protected override void Init()
+    {
+        ModuleConfig = LoadConfig<Config>() ?? new();
+
+        FrameworkManager.Reg(OnUpdate, throttleMS: 5_000);
+    }
+
+    protected override void Uninit() =>
+        FrameworkManager.Unreg(OnUpdate);
+
+    protected override void ConfigUI()
+    {
+        using var tab = ImRaii.TabBar("TabBar");
+        if (!tab) return;
+
+        using (var item = ImRaii.TabItem(GetLoc("OutOfDuty")))
+        {
+            if (item)
+            {
+                ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), GetLoc("AutoChangeBattleEffectLevel-PlayerThreshold"));
+
+                using (ImRaii.PushIndent())
+                {
+                    ImGui.SetNextItemWidth(100f * GlobalFontScale);
+                    ImGui.InputUInt($"{LuminaWrapper.GetAddonText(16347)}##LimitLow", ref ModuleConfig.AroundCountThresholdLow);
+                    if (ImGui.IsItemDeactivatedAfterEdit())
+                        SaveConfig(ModuleConfig);
+
+                    ImGui.SetNextItemWidth(100f * GlobalFontScale);
+                    ImGui.InputUInt($"{LuminaWrapper.GetAddonText(16346)}##LimitHigh", ref ModuleConfig.AroundCountThresholdHigh);
+                    if (ImGui.IsItemDeactivatedAfterEdit())
+                        SaveConfig(ModuleConfig);
+                }
+        
+                ImGui.NewLine();
+
+                if (ImGui.CollapsingHeader($"＜ {DService.SeStringEvaluator.EvaluateFromAddon(12871, [ModuleConfig.AroundCountThresholdLow])}",
+                                           ImGuiTreeNodeFlags.DefaultOpen))
+                    DrawBattleEffectSetting("Low", ModuleConfig.OverworldLow);
+
+                if (ImGui.CollapsingHeader($"{DService.SeStringEvaluator.EvaluateFromAddon(12871, [ModuleConfig.AroundCountThresholdLow])}" +
+                                           $" ≤ X ≤ "                                                                                       +
+                                           $"{DService.SeStringEvaluator.EvaluateFromAddon(12871, [ModuleConfig.AroundCountThresholdHigh])}",
+                                           ImGuiTreeNodeFlags.DefaultOpen))
+                    DrawBattleEffectSetting("Medium", ModuleConfig.OverworldMedium);
+
+                if (ImGui.CollapsingHeader($"＞ {DService.SeStringEvaluator.EvaluateFromAddon(12871, [ModuleConfig.AroundCountThresholdHigh])}",
+                                           ImGuiTreeNodeFlags.DefaultOpen))
+                    DrawBattleEffectSetting("High", ModuleConfig.OverworldHigh);
+            }
+        }
+
+        using (var item = ImRaii.TabItem(GetLoc("InDuty")))
+        {
+            if (item)
+            {
+                ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), GetLoc("Default"));
+
+                using (ImRaii.PushIndent())
+                    DrawBattleEffectSetting("DefaultDuty", ModuleConfig.DutyDefault);
+                
+                ImGui.NewLine();
+                
+                foreach (var contentType in LuminaGetter.Get<ContentType>())
+                {
+                    var name = contentType.Name.ToString();
+                    if (string.IsNullOrEmpty(name)) continue;
+            
+                    if (ModuleConfig.DutySpecific.TryAdd(contentType.RowId, new()))
+                        ModuleConfig.Save(this);
+            
+                    var setting = ModuleConfig.DutySpecific[contentType.RowId];
+
+                    if (!ImageHelper.TryGetGameIcon(contentType.Icon, out var image)) continue;
+
+                    if (ImGuiOm.TreeNodeImageWithText(image.Handle, new(ImGui.GetTextLineHeightWithSpacing()), $"{name} ({contentType.RowId})"))
+                    {
+                        DrawBattleEffectSetting($"Duty_{contentType.RowId}", setting);
+                        ImGui.TreePop();
+                    }
+                }
+            }
+        }
+    }
+
+    private void DrawBattleEffectSetting(string id, EffectSetting setting)
+    {
+        using var idPush = ImRaii.PushId(id);
+        
+        var isEnabled = setting.IsEnabled;
+        if (ImGuiComponents.ToggleButton("Enable", ref isEnabled))
+        {
+            setting.IsEnabled = isEnabled;
+            ModuleConfig.Save(this);
+        }
+        
+        ImGui.SameLine();
+        ImGui.AlignTextToFramePadding();
+        ImGui.Text(GetLoc("Enable"));
+        
+        if (!isEnabled) return;
+        
+        ImGui.Spacing();
+        ImGui.Spacing();
+        
+        var selfSetting = setting.Self;
+        if (DrawBattleEffectLevelCombo(LuminaWrapper.GetAddonText(4087), ref selfSetting))
+        {
+            setting.Self = selfSetting;
+            ModuleConfig.Save(this);
+        }
+        
+        ImGui.Spacing();
+
+        var partySetting = setting.Party;
+        if (DrawBattleEffectLevelCombo(LuminaWrapper.GetAddonText(4088), ref partySetting))
+        {
+            setting.Party = partySetting;
+            ModuleConfig.Save(this);
+        }
+        
+        ImGui.Spacing();
+        
+        var otherSetting = setting.Other;
+        if (DrawBattleEffectLevelCombo(LuminaWrapper.GetAddonText(4089), ref otherSetting))
+        {
+            setting.Other = otherSetting;
+            ModuleConfig.Save(this);
+        }
+        
+        ImGui.Spacing();
+        
+        var enemySetting = setting.Enemy;
+        if (DrawBattleEffectLevelCombo(LuminaWrapper.GetAddonText(4109), ref enemySetting))
+        {
+            setting.Enemy = enemySetting;
+            ModuleConfig.Save(this);
+        }
+    }
+
+    private static bool DrawBattleEffectLevelCombo(string label, ref BattleEffectLevel value)
+    {
+        var returnValue = false;
+        
+        using var id = ImRaii.PushId(label);
+
+        ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), label);
+        
+        using var indent = ImRaii.PushIndent();
+        
+        ImGui.Spacing();
+        foreach (var level in Enum.GetValues<BattleEffectLevel>())
+        {
+            ImGui.SameLine(0, 10f * GlobalFontScale);
+            if (ImGui.RadioButton(LuminaWrapper.GetAddonText((uint)level + 7823), level == value))
+            {
+                value       = level;
+                returnValue = true;
+            }
+        }
+
+        return returnValue;
+    }
+
+    private static void OnUpdate(IFramework framework)
+    {
+        EffectSetting? targetSetting = null;
+        if (GameState.ContentFinderCondition > 0)
+        {
+            if (ModuleConfig.DutySpecific.TryGetValue(GameState.ContentFinderConditionData.ContentType.RowId, out var specificConfig) &&
+                specificConfig.IsEnabled)
+                targetSetting = specificConfig;
+
+            targetSetting ??= ModuleConfig.DutyDefault;
+        }
+        else
+        {
+            var playerCount = PlayersManager.PlayersAroundCount;
+
+            if (playerCount < ModuleConfig.AroundCountThresholdLow)
+                targetSetting = ModuleConfig.OverworldLow;
+            else if (playerCount < ModuleConfig.AroundCountThresholdHigh)
+                targetSetting = ModuleConfig.OverworldMedium;
+            else
+                targetSetting = ModuleConfig.OverworldHigh;
+        }
+
+        if (targetSetting is not { IsEnabled: true }) return;
+        
+        ApplySetting(targetSetting);
+    }
+
+    private static void ApplySetting(EffectSetting? settings)
+    {
+        if (settings == null) return;
+        if (LastAppliedSettings != null && settings == LastAppliedSettings)
+            return;
+
+        try
+        {
+            DService.GameConfig.UiConfig.Set(nameof(UiConfigOption.BattleEffectSelf),       (uint)settings.Self);
+            DService.GameConfig.UiConfig.Set(nameof(UiConfigOption.BattleEffectParty),      (uint)settings.Party);
+            DService.GameConfig.UiConfig.Set(nameof(UiConfigOption.BattleEffectOther),      (uint)settings.Other);
+            DService.GameConfig.UiConfig.Set(nameof(UiConfigOption.BattleEffectPvPEnemyPc), (uint)settings.Enemy);
+
+            LastAppliedSettings = settings.Clone();
+        }
+        catch
+        {
+            // ignored
+        }
+    }
+    
+    private class Config : ModuleConfiguration
+    {
+        public uint AroundCountThresholdLow  = 20;
+        public uint AroundCountThresholdHigh = 40;
+
+        public EffectSetting OverworldLow  = new()
+        {
+            Self  = BattleEffectLevel.All, 
+            Party = BattleEffectLevel.All, 
+            Other = BattleEffectLevel.All, 
+            Enemy = BattleEffectLevel.All
+        };
+        
+        public EffectSetting OverworldMedium  = new()
+        {
+            Self  = BattleEffectLevel.All, 
+            Party = BattleEffectLevel.Limited,
+            Other = BattleEffectLevel.None,
+            Enemy = BattleEffectLevel.All
+        };
+        
+        public EffectSetting OverworldHigh = new()
+        {
+            Self  = BattleEffectLevel.All,
+            Party = BattleEffectLevel.None, 
+            Other = BattleEffectLevel.None, 
+            Enemy = BattleEffectLevel.All
+        };
+        
+        public EffectSetting DutyDefault = new()
+        {
+            Self  = BattleEffectLevel.All,
+            Party = BattleEffectLevel.All, 
+            Other = BattleEffectLevel.Limited, 
+            Enemy = BattleEffectLevel.All
+        };
+
+        public Dictionary<uint, EffectSetting> DutySpecific = [];
+    }
+    
+    private sealed class EffectSetting : IEquatable<EffectSetting>
+    {
+        public bool IsEnabled { get; set; }
+
+        /// <summary>
+        /// 自己
+        /// </summary>
+        public BattleEffectLevel Self { get; set; }
+
+        /// <summary>
+        /// 小队
+        /// </summary>
+        public BattleEffectLevel Party { get; set; } = BattleEffectLevel.None;
+
+        /// <summary>
+        /// 他人
+        /// </summary>
+        public BattleEffectLevel Other { get; set; } = BattleEffectLevel.None;
+
+        /// <summary>
+        /// 对战时的敌方玩家
+        /// </summary>
+        public BattleEffectLevel Enemy { get; set; }
+
+        public EffectSetting Clone() =>
+            (EffectSetting)MemberwiseClone();
+
+        public bool Equals(EffectSetting? other)
+        {
+            if (other is null) return false;
+            return Self  == other.Self  &&
+                   Party == other.Party &&
+                   Other == other.Other &&
+                   Enemy == other.Enemy;
+        }
+
+        public override bool Equals(object? obj) => 
+            Equals(obj as EffectSetting);
+
+        public override int GetHashCode() => 
+            HashCode.Combine(Self, Party, Other, Enemy);
+
+        public static bool operator ==(EffectSetting? left, EffectSetting? right) => 
+            Equals(left, right);
+
+        public static bool operator !=(EffectSetting? left, EffectSetting? right) => 
+            !Equals(left, right);
+    }
+    
+    private enum BattleEffectLevel : uint
+    {
+        /// <summary>
+        /// 完全显示
+        /// </summary>
+        All,
+        
+        /// <summary>
+        /// 简单显示
+        /// </summary>
+        Limited,
+        
+        /// <summary>
+        /// 不显示
+        /// </summary>
+        None
+    }
+}
