454af1ad23719e39477ad3cff10dc472f8e1b053
AtmoOmen <atmoomen2004@outlook.com>
2026-01-11 16:02:05 +0800

    SelectableRecruitmentText

--- Recruitment/SelectableRecruitmentText.cs
+++ Recruitment/SelectableRecruitmentText.cs
@@ -2,7 +2,10 @@ using System.Numerics;
 using DailyRoutines.Abstracts;
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using Dalamud.Utility;
+using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit.Nodes;
 using Lumina.Text.ReadOnly;
 
@@ -22,7 +25,7 @@ public unsafe class SelectableRecruitmentText : DailyModuleBase
     
     protected override void Init()
     {
-        DService.Instance().AddonLifecycle.RegisterListener(AddonEvent.PreRefresh, "LookingForGroupDetail", OnAddon);
+        DService.Instance().AddonLifecycle.RegisterListener(AddonEvent.PreDraw,     "LookingForGroupDetail", OnAddon);
         DService.Instance().AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "LookingForGroupDetail", OnAddon);
     }
     
@@ -39,14 +42,28 @@ public unsafe class SelectableRecruitmentText : DailyModuleBase
             case AddonEvent.PreDraw:
                 if (LookingForGroupDetail == null) return;
 
+                var agent = AgentLookingForGroup.Instance();
+                if (agent == null) return;
+                
                 var origText = LookingForGroupDetail->GetTextNodeById(20);
                 if (origText == null) return;
                 
                 if (RecruitmentTextNode != null)
                 {
-                    if (!RecruitmentTextNode.IsFocused && string.IsNullOrEmpty(RecruitmentTextNode.String))
-                        RecruitmentTextNode.SeString = new ReadOnlySeStringSpan(AgentLookingForGroup.Instance()->LastViewedListing.Comment).PraseAutoTranslate();
-
+                    var formatAddon = (AddonLookingForGroupDetail*)LookingForGroupDetail;
+                    if (formatAddon->PartyLeaderTextNode->NodeText.StringPtr.ExtractText() !=
+                        agent->LastViewedListing.LeaderString)
+                        return;
+                    
+                    if (RecruitmentTextNode is { IsFocused: false, SeString.IsEmpty: true })
+                    {
+                        var seString = new ReadOnlySeStringSpan(agent->LastViewedListing.Comment).PraseAutoTranslate().ToDalamudString();
+                        RecruitmentTextNode.SeString = seString.Encode();
+                    }
+                    
+                    if (RecruitmentTextNode is { IsVisible: false, SeString.IsEmpty: false })
+                        RecruitmentTextNode.IsVisible = true;
+                    
                     return;
                 }
 
@@ -66,8 +83,11 @@ public unsafe class SelectableRecruitmentText : DailyModuleBase
                     Size             = new(520, 60),
                     Position         = new Vector2(origButton->OwnerNode->X, origButton->OwnerNode->Y) - new Vector2(10, 8),
                     ShowLimitText    = false,
+                    IsVisible        = false,
+                    MaxLines         = 2,
                 };
                 RecruitmentTextNode.TextLimitsNode.DetachNode();
+                RecruitmentTextNode.CurrentTextNode.TextFlags |= TextFlags.WordWrap;
                 RecruitmentTextNode.AttachNode(LookingForGroupDetail);
 
                 break;
