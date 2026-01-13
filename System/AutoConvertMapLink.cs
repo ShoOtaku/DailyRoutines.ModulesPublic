2a1a50b6b868106f301c59b98de4113e715e7262
AtmoOmen <atmoomen2004@outlook.com>
2026-01-13 23:31:16 +0800

    AutoConvertMapLink

--- System/AutoConvertMapLink.cs
+++ System/AutoConvertMapLink.cs
@@ -1,8 +1,3 @@
-using DailyRoutines.Abstracts;
-using Dalamud.Game.Text.SeStringHandling;
-using Dalamud.Game.Text.SeStringHandling.Payloads;
-using Dalamud.Hooking;
-using Lumina.Excel.Sheets;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -10,11 +5,16 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
+using DailyRoutines.Abstracts;
+using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Game.Text.SeStringHandling.Payloads;
+using Dalamud.Hooking;
+using Lumina.Excel.Sheets;
 
-namespace DailyRoutines.Modules;
+namespace DailyRoutines.ModulesPublic;
 
 // From Asvel
-public class AutoConvertMapLink : DailyModuleBase
+public partial class AutoConvertMapLink : DailyModuleBase
 {
     public override ModuleInfo Info { get; } = new()
     {
@@ -28,32 +28,29 @@ public class AutoConvertMapLink : DailyModuleBase
     private delegate        nint                        MessageParseDelegate(nint a, nint b);
     private static          Hook<MessageParseDelegate>? MessageParseHook;
 
-    private readonly Regex mapLinkPattern =
-        new(@"\uE0BB(?<map>.+?)(?<instance>[\ue0b1-\ue0b9])? \( (?<x>\d{1,2}\.\d)  , (?<y>\d{1,2}\.\d) \)", RegexOptions.Compiled);
-    
-    private static readonly Random random = new();
-
     protected override void Init()
     {
         MessageParseHook ??= MessageParseSig.GetHook<MessageParseDelegate>(ParseMessageDetour);
         MessageParseHook.Enable();
     }
 
-    private nint ParseMessageDetour(nint a, nint b)
+    private static nint ParseMessageDetour(nint a, nint b)
     {
         var ret = MessageParseHook.Original(a, b);
+
         try
         {
             var pMessage = Marshal.ReadIntPtr(ret);
             var length   = 0;
-            
-            while (Marshal.ReadByte(pMessage, length) != 0) 
+
+            while (Marshal.ReadByte(pMessage, length) != 0)
                 length++;
-            
+
             var message = new byte[length];
             Marshal.Copy(pMessage, message, 0, length);
 
             var parsed = SeString.Parse(message);
+
             foreach (var payload in parsed.Payloads)
             {
                 if (payload is AutoTranslatePayload p && p.Encode()[3] == 0xC9 && p.Encode()[4] == 0x04)
@@ -63,7 +60,7 @@ public class AutoConvertMapLink : DailyModuleBase
             for (var i = 0; i < parsed.Payloads.Count; i++)
             {
                 if (parsed.Payloads[i] is not TextPayload payload) continue;
-                var match = mapLinkPattern.Match(payload.Text);
+                var match = MapLinkRegex().Match(payload.Text);
                 if (!match.Success) continue;
 
                 var mapName = match.Groups["map"].Value;
@@ -77,14 +74,14 @@ public class AutoConvertMapLink : DailyModuleBase
 
                 var rawX = GenerateRawPosition(float.Parse(match.Groups["x"].Value, CultureInfo.InvariantCulture), map.OffsetX, map.SizeFactor);
                 var rawY = GenerateRawPosition(float.Parse(match.Groups["y"].Value, CultureInfo.InvariantCulture), map.OffsetY, map.SizeFactor);
-                if (match.Groups["instance"].Value != "") 
-                    mapId |= (match.Groups["instance"].Value[0] - 0xe0b0u) << 16;
+                if (match.Groups["instance"].Value != "")
+                    mapId |= match.Groups["instance"].Value[0] - 0xe0b0u << 16;
 
                 var newPayloads = new List<Payload>();
-                if (match.Index > 0) 
+                if (match.Index > 0)
                     newPayloads.Add(new TextPayload(payload.Text[..match.Index]));
                 newPayloads.Add(new PreMapLinkPayload(territoryId, mapId, rawX, rawY));
-                if (match.Index + match.Length < payload.Text.Length) 
+                if (match.Index + match.Length < payload.Text.Length)
                     newPayloads.Add(new TextPayload(payload.Text[(match.Index + match.Length)..]));
                 parsed.Payloads.RemoveAt(i);
                 parsed.Payloads.InsertRange(i, newPayloads);
@@ -106,20 +103,20 @@ public class AutoConvertMapLink : DailyModuleBase
 
         return ret;
     }
-    
-    private int GenerateRawPosition(float visibleCoordinate, short offset, ushort factor)
+
+    private static int GenerateRawPosition(float visibleCoordinate, short offset, ushort factor)
     {
-        visibleCoordinate += (float)random.NextDouble() * 0.07f;
-        var scale     = factor                                                             / 100.0f;
-        var scaledPos = (((visibleCoordinate - 1.0f) * scale / 41.0f * 2048.0f) - 1024.0f) / scale;
-        return (int)Math.Ceiling(scaledPos                                      - offset) * 1000;
+        visibleCoordinate += (float)Random.Shared.NextDouble() * 0.07f;
+        var scale     = factor                                                           / 100.0f;
+        var scaledPos = ((visibleCoordinate - 1.0f) * scale / 41.0f * 2048.0f - 1024.0f) / scale;
+        return (int)Math.Ceiling(scaledPos                                    - offset) * 1000;
     }
 
     private class PreMapLinkPayload(uint zoneID, uint mapID, int rawX, int rawY) : Payload
     {
         public override PayloadType Type => PayloadType.AutoTranslateText;
 
-        private readonly int rawZ = -30000;
+        private const int RAW_Z = -30000;
 
         protected override byte[] EncodeImpl()
         {
@@ -127,7 +124,7 @@ public class AutoConvertMapLink : DailyModuleBase
             var mapBytes       = MakeInteger(mapID);
             var xBytes         = MakeInteger(unchecked((uint)rawX));
             var yBytes         = MakeInteger(unchecked((uint)rawY));
-            var zBytes         = MakeInteger(unchecked((uint)rawZ));
+            var zBytes         = MakeInteger(unchecked((uint)RAW_Z));
 
             var chunkLen = 4 + territoryBytes.Length + mapBytes.Length + xBytes.Length + yBytes.Length + zBytes.Length;
 
@@ -136,7 +133,7 @@ public class AutoConvertMapLink : DailyModuleBase
                 START_BYTE,
                 (byte)SeStringChunkType.AutoTranslateKey, (byte)chunkLen, 0xC9, 0x04
             };
-            
+
             bytes.AddRange(territoryBytes);
             bytes.AddRange(mapBytes);
             bytes.AddRange(xBytes);
@@ -150,4 +147,7 @@ public class AutoConvertMapLink : DailyModuleBase
 
         protected override void DecodeImpl(BinaryReader reader, long endOfStream) => throw new NotImplementedException();
     }
+
+    [GeneratedRegex(@"\uE0BB(?<map>.+?)(?<instance>[\ue0b1-\ue0b9])? \( (?<x>\d{1,2}\.\d)  , (?<y>\d{1,2}\.\d) \)", RegexOptions.Compiled)]
+    private static partial Regex MapLinkRegex();
 }
