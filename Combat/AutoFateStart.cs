378ca0232fcd1228380e5ae00e4134daad4b39ff
AtmoOmen <atmoomen2004@outlook.com>
2026-01-13 13:03:16 +0800

    AutoFateStart

--- Combat/AutoFateStart.cs
+++ Combat/AutoFateStart.cs
@@ -11,34 +11,58 @@ public class AutoFateStart : DailyModuleBase
     {
         Title       = GetLoc("AutoFateStartTitle"),
         Description = GetLoc("AutoFateStartDescription"),
-        Category    = ModuleCategories.Combat,
+        Category    = ModuleCategories.Combat
     };
 
     public override ModulePermission Permission { get; } = new() { NeedAuth = true };
-    
-    protected override void Init() => 
-        FrameworkManager.Instance().Reg(OnUpdate, throttleMS: 2_000);
+
+    protected override void Init()
+    {
+        DService.Instance().ClientState.TerritoryChanged += OnZoneChanged;
+        OnZoneChanged(0);
+    }
+
+    protected override void Uninit()
+    {
+        DService.Instance().ClientState.TerritoryChanged -= OnZoneChanged;
+        FrameworkManager.Instance().Unreg(OnUpdate);
+    }
+
+    private static void OnZoneChanged(ushort obj)
+    {
+        FrameworkManager.Instance().Unreg(OnUpdate);
+
+        if (GameState.TerritoryIntendedUse != TerritoryIntendedUse.Overworld || GameState.IsInPVPArea)
+            return;
+
+        FrameworkManager.Instance().Reg(OnUpdate, 2_000);
+    }
 
     private static void OnUpdate(IFramework _)
     {
-        if (GameState.TerritoryIntendedUse != TerritoryIntendedUse.Overworld ||
-            GameState.IsInPVPArea                                            ||
-            LocalPlayerState.ClassJobData.DohDolJobIndex != -1)
-            return;
-            
-        foreach (var obj in DService.Instance().ObjectTable.CharacterManagerObjects)
+        if (GameState.TerritoryIntendedUse != TerritoryIntendedUse.Overworld || GameState.IsInPVPArea)
         {
-            if (obj is not IBattleNPC { NamePlateIconID: 60093 } battleNPC                        ||
-                LuminaGetter.GetRow<Fate>(battleNPC.FateID) is not { ClassJobLevel: > 0 } fateRow ||
-                !Throttler.Throttle($"AutoFateStart-Fate-{battleNPC.FateID}", 60_000))
-                continue;
-            
+            FrameworkManager.Instance().Unreg(OnUpdate);
+            return;
+        }
+
+        if (LocalPlayerState.ClassJobData.DohDolJobIndex != -1)
+            return;
+
+        var objs = DService.Instance().ObjectTable.SearchObjects
+        (obj =>
+             obj is IBattleNPC { NamePlateIconID: 60093 } battleNPC                &&
+             LuminaGetter.GetRow<Fate>(battleNPC.FateID) is { ClassJobLevel: > 0 } &&
+             Throttler.Throttle($"AutoFateStart-Fate-{battleNPC.FateID}", 60_000)
+        );
+
+        foreach (var o in objs)
+        {
+            var battleNPC = o as IBattleNPC;
+
             ExecuteCommandManager.Instance().ExecuteCommand(ExecuteCommandFlag.FateStart, battleNPC.FateID, battleNPC.EntityID);
-            Chat(GetLoc("AutoFateStart-StartNotice", fateRow.Name.ToString(), battleNPC.Name.ToString()));
+            Chat(GetLoc("AutoFateStart-StartNotice", LuminaWrapper.GetFateName(battleNPC.FateID), battleNPC.Name.ToString()));
             break;
         }
     }
-
-    protected override void Uninit() =>
-        FrameworkManager.Instance().Unreg(OnUpdate);
 }
