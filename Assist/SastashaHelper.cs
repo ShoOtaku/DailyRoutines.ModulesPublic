477d62910ce48f3ae55dc90665d94eba0718783d
AtmoOmen <atmoomen2004@outlook.com>
2026-01-14 16:29:01 +0800

    SastashaHelper

--- Assist/SastashaHelper.cs
+++ Assist/SastashaHelper.cs
@@ -1,17 +1,14 @@
 using System.Collections.Generic;
-using System.Linq;
 using DailyRoutines.Abstracts;
 using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Plugin.Services;
 using FFXIVClientStructs.FFXIV.Client.Game.Object;
 using FFXIVClientStructs.FFXIV.Client.UI;
-using Lumina.Excel.Sheets;
-using OmenTools.Extensions;
 using ObjectKind = Dalamud.Game.ClientState.Objects.Enums.ObjectKind;
 
 namespace DailyRoutines.ModulesPublic;
 
-public class SastashaHelper : DailyModuleBase
+public unsafe class SastashaHelper : DailyModuleBase
 {
     public override ModuleInfo Info { get; } = new()
     {
@@ -19,7 +16,7 @@ public class SastashaHelper : DailyModuleBase
         Description = GetLoc("SastashaHelperDescription"),
         Category    = ModuleCategories.Assist
     };
-    
+
     public override ModulePermission Permission { get; } = new() { AllDefaultEnabled = true };
 
     // Book Data ID - Coral Data ID
@@ -30,68 +27,93 @@ public class SastashaHelper : DailyModuleBase
         // 红珊瑚
         [2001548] = (2000214, 17, ObjectHighlightColor.Green),
         // 绿珊瑚
-        [2001549] = (2000215, 45, ObjectHighlightColor.Red),
+        [2001549] = (2000215, 45, ObjectHighlightColor.Red)
     };
 
-    private static ulong CorrectCoralDataID;
+    private static ulong                CorrectCoralDataID;
     private static ObjectHighlightColor CorrectCoralHighlightColor;
 
     protected override void Init()
     {
         TaskHelper ??= new() { TimeoutMS = 30_000 };
-        
+
         DService.Instance().ClientState.TerritoryChanged += OnZoneChanged;
         OnZoneChanged(0);
     }
+    
+    protected override void Uninit()
+    {
+        DService.Instance().ClientState.TerritoryChanged -= OnZoneChanged;
+        FrameworkManager.Instance().Unreg(OnUpdate);
+        GamePacketManager.Instance().Unreg(OnPostSendPackt);
 
+        CorrectCoralDataID         = 0;
+        CorrectCoralHighlightColor = ObjectHighlightColor.None;
+    }
+    
     private void OnZoneChanged(ushort zone)
     {
         TaskHelper?.Abort();
         FrameworkManager.Instance().Unreg(OnUpdate);
+        GamePacketManager.Instance().Unreg(OnPostSendPackt);
 
-        CorrectCoralDataID = 0;
+        CorrectCoralDataID         = 0;
         CorrectCoralHighlightColor = ObjectHighlightColor.None;
+        
         if (GameState.TerritoryType != 1036) return;
         
         TaskHelper.Enqueue(GetCorrectCoral);
-        FrameworkManager.Instance().Reg(OnUpdate, throttleMS: 2_000);
+        GamePacketManager.Instance().RegPostSendPacket(OnPostSendPackt);
+        FrameworkManager.Instance().Reg(OnUpdate, 2_000);
     }
 
-    private static unsafe void OnUpdate(IFramework _)
+    private static void OnPostSendPackt(int opcode, byte* packet, ushort priority)
+    {
+        if (opcode != GamePacketOpcodes.EventStartOpcode) return;
+        
+        var packetData = (EventStartPackt*)packet;
+        if (packetData->EventID == 983066)
+            FrameworkManager.Instance().Unreg(OnUpdate);
+    }
+    
+    private static void OnUpdate(IFramework _)
     {
         if (CorrectCoralDataID == 0 || CorrectCoralHighlightColor == ObjectHighlightColor.None) return;
 
-        var coral = DService.Instance().ObjectTable.FirstOrDefault(
-            x => x.ObjectKind == ObjectKind.EventObj && x.DataID == CorrectCoralDataID);
-        if (coral == null) return;
+        if (DService.Instance().ObjectTable.SearchObject
+                (x => x.ObjectKind == ObjectKind.EventObj && x.DataID == CorrectCoralDataID) is not { } coral)
+            return;
 
         coral.ToStruct()->Highlight(coral.IsTargetable ? CorrectCoralHighlightColor : ObjectHighlightColor.None);
     }
-    
+
     private static bool GetCorrectCoral()
     {
-        if (DService.Instance().ObjectTable.LocalPlayer is null || BetweenAreas || !UIModule.IsScreenReady()) return false;
-        
+        if (!UIModule.IsScreenReady()) return false;
+
         var book = DService.Instance().ObjectTable
-                           .FirstOrDefault(x => x is { IsTargetable: true, ObjectKind: ObjectKind.EventObj } && 
-                                                BookToCoral.ContainsKey(x.DataID));
+                           .SearchObject
+                           (
+                               x => x is { IsTargetable: true, ObjectKind: ObjectKind.EventObj } && BookToCoral.ContainsKey(x.DataID),
+                               IObjectTable.EventRange
+                           );
         if (book == null) return false;
 
         var info = BookToCoral[book.DataID];
 
-        Chat(GetSLoc("SastashaHelper-Message",
-                     new SeStringBuilder()
-                         .AddUiForeground(LuminaGetter.GetRow<EObjName>(info.CoralDataID)!.Value.Singular.ToString(),
-                                          info.UIColor).Build()));
-        
+        Chat
+        (
+            GetSLoc
+            (
+                "SastashaHelper-Message",
+                new SeStringBuilder()
+                    .AddUiForeground(LuminaWrapper.GetEObjName(info.CoralDataID), info.UIColor)
+                    .Build()
+            )
+        );
+
         CorrectCoralDataID         = info.CoralDataID;
         CorrectCoralHighlightColor = info.HighlightColor;
         return true;
-    }
-
-    protected override void Uninit()
-    {
-        DService.Instance().ClientState.TerritoryChanged -= OnZoneChanged;
-        OnZoneChanged(0);
     }
 }
