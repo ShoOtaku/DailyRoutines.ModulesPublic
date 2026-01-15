6a6e80488b1c7e412953185377d3c457ceee20b5
AtmoOmen <atmoomen2004@outlook.com>
2026-01-15 13:19:55 +0800

    AutoTimedLogout

--- General/AutoTimedLogout.cs
+++ General/AutoTimedLogout.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 using DailyRoutines.Abstracts;
-using DailyRoutines.Managers;
-using Dalamud.Interface;
-using Dalamud.Plugin.Services;
 using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using Task = System.Threading.Tasks.Task;
 
 namespace DailyRoutines.ModulesPublic;
 
@@ -15,7 +15,7 @@ public class AutoTimedLogout : DailyModuleBase
     {
         Title               = GetLoc("AutoTimedLogoutTitle"),
         Description         = GetLoc("AutoTimedLogoutDescription"),
-        Category            = ModuleCategories.System,
+        Category            = ModuleCategories.General,
         ModulesPrerequisite = ["InstantLogout"],
         Author              = ["Wotou"]
     };
@@ -24,18 +24,19 @@ public class AutoTimedLogout : DailyModuleBase
     {
         [OperationMode.Logout]       = GetLoc("AutoTimedLogout-Mode-Logout"),
         [OperationMode.ShutdownGame] = GetLoc("AutoTimedLogout-Mode-ShutdownGame"),
-        [OperationMode.ShutdownPC]   = GetLoc("AutoTimedLogout-Mode-ShutdownPC"),
+        [OperationMode.ShutdownPC]   = GetLoc("AutoTimedLogout-Mode-ShutdownPC")
     };
 
-    private static int           CustomMinutes = 30;
-    private static long?         ScheduledTime;
-    private static OperationMode CurrentOperation = OperationMode.Logout;
+    private static int                      CustomMinutes = 30;
+    private static long?                    ScheduledTime;
+    private static OperationMode            CurrentOperation = OperationMode.Logout;
+    private static CancellationTokenSource? CancelSource;
 
-    protected override void Init()
-    {
+    protected override void Init() =>
         Abort();
-        FrameworkManager.Instance().Reg(OnUpdate, throttleMS: 1_000);
-    }
+
+    protected override void Uninit() =>
+        Abort();
 
     protected override void ConfigUI()
     {
@@ -52,35 +53,36 @@ public class AutoTimedLogout : DailyModuleBase
 
                 var operationText = ModeLoc.GetValueOrDefault(CurrentOperation, string.Empty);
                 ImGui.TextColored(KnownColor.GreenYellow.ToVector4(), $"{operationText}:");
-                
+
                 ImGui.SameLine();
                 ImGui.TextUnformatted($" {hours:D2}:{minutes:D2}:{seconds:D2}");
             }
 
             if (ImGuiOm.ButtonIconWithText(FontAwesomeIcon.Times, GetLoc("Cancel")))
                 Abort();
-            
+
             return;
         }
-        
+
         ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), GetLoc("Operation"));
-        
+
         using (ImRaii.PushIndent())
         {
             var isFirst = true;
+
             foreach (var (operationMode, loc) in ModeLoc)
             {
                 if (!isFirst)
                     ImGui.SameLine();
                 isFirst = false;
-                
+
                 if (ImGui.RadioButton(loc, CurrentOperation == operationMode))
                     CurrentOperation = operationMode;
             }
         }
-        
+
         ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), GetLoc("Time"));
-        
+
         using (ImRaii.PushIndent())
         {
             ImGui.SetNextItemWidth(150f * GlobalFontScale);
@@ -101,75 +103,101 @@ public class AutoTimedLogout : DailyModuleBase
             ImGui.SameLine();
             if (ImGui.Button($"3 {GetLoc("Hour")}"))
                 CustomMinutes = 180;
-            
+
             ImGui.SameLine();
             if (ImGui.Button($"6 {GetLoc("Hour")}"))
                 CustomMinutes = 360;
-            
+
             ImGui.SameLine();
             if (ImGui.Button($"12 {GetLoc("Hour")}"))
                 CustomMinutes = 720;
-            
+
             ImGui.SameLine();
             if (ImGui.Button($"24 {GetLoc("Hour")}"))
                 CustomMinutes = 1440;
         }
-        
+
         ImGui.Spacing();
 
         if (ImGuiOm.ButtonIconWithText(FontAwesomeIcon.Check, GetLoc("Confirm")))
             StartWithMinutes(CustomMinutes, CurrentOperation);
     }
 
-    private static void OnUpdate(IFramework framework)
-    {
-        if (!ScheduledTime.HasValue || Framework.GetServerTime() < ScheduledTime.Value) return;
-        ScheduledTime = null;
-
-        switch (CurrentOperation)
-        {
-            case OperationMode.Logout:
-                ChatManager.Instance().SendMessage("/logout");
-                break;
-            case OperationMode.ShutdownGame:
-                ChatManager.Instance().SendMessage("/shutdown");
-                break;
-            case OperationMode.ShutdownPC:
-                try
-                {
-                    Process.Start(new ProcessStartInfo()
-                    {
-                        FileName        = "shutdown",
-                        Arguments       = "/s /t 0",
-                        UseShellExecute = false,
-                        CreateNoWindow  = true
-                    });
-                }
-                catch (Exception ex)
-                {
-                    Error($"尝试自动关闭电脑失败: {ex.Message}", ex);
-                }
-
-                break;
-        }
-    }
-
     private static void StartWithMinutes(int minutes, OperationMode operation)
     {
         Abort();
         CurrentOperation = operation;
-        ScheduledTime    = Framework.GetServerTime() + (minutes * 60);
+        ScheduledTime    = Framework.GetServerTime() + minutes * 60;
+
+        CancelSource = new();
+        _            = WaitForTimer(minutes * 60 * 1000, CancelSource.Token);
     }
 
-    private static void Abort() =>
+    private static async Task WaitForTimer(int delayMs, CancellationToken token)
+    {
+        try
+        {
+            await Task.Delay(delayMs, token);
+            if (token.IsCancellationRequested) return;
+
+            ScheduledTime = null;
+
+            switch (CurrentOperation)
+            {
+                case OperationMode.ShutdownPC:
+                    ExecuteShutdownPC();
+                    break;
+
+                case OperationMode.Logout:
+                    await DService.Instance().Framework.RunOnFrameworkThread(() => ChatManager.Instance().SendMessage("/logout"));
+                    break;
+
+                case OperationMode.ShutdownGame:
+                    await DService.Instance().Framework.RunOnFrameworkThread(() => ChatManager.Instance().SendMessage("/shutdown"));
+                    break;
+            }
+        }
+        catch (TaskCanceledException)
+        {
+            // ignored
+        }
+    }
+
+    private static void ExecuteShutdownPC()
+    {
+        try
+        {
+            Process.Start
+            (
+                new ProcessStartInfo
+                {
+                    FileName        = "shutdown",
+                    Arguments       = "/s /t 0",
+                    UseShellExecute = false,
+                    CreateNoWindow  = true
+                }
+            );
+        }
+        catch (Exception ex)
+        {
+            Error($"尝试自动关闭电脑失败: {ex.Message}", ex);
+        }
+    }
+
+    private static void Abort()
+    {
         ScheduledTime = null;
 
-    protected override void Uninit()
-    {
-        FrameworkManager.Instance().Unreg(OnUpdate);
-        Abort();
+        if (CancelSource != null)
+        {
+            if (!CancelSource.IsCancellationRequested)
+                CancelSource.Cancel();
+
+            CancelSource.Dispose();
+            CancelSource = null;
+        }
     }
-    
+
     public enum OperationMode
     {
         Logout,
