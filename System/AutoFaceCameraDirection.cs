db8347b32257ad2bacdcca534f1e397f6df9aa3f
AtmoOmen <atmoomen2004@outlook.com>
2026-01-15 11:56:23 +0800

    AutoFaceCameraDirection 格式小调整

--- System/AutoFaceCameraDirection.cs
+++ System/AutoFaceCameraDirection.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Frozen;
 using System.Collections.Generic;
 using System.Numerics;
 using DailyRoutines.Abstracts;
@@ -6,9 +7,7 @@ using DailyRoutines.Managers;
 using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Hooking;
 using Dalamud.Interface.Components;
-using Dalamud.Plugin.Services;
 using FFXIVClientStructs.FFXIV.Client.Game;
-using FFXIVClientStructs.FFXIV.Client.Game.Control;
 using Camera = FFXIVClientStructs.FFXIV.Client.Game.Camera;
 
 namespace DailyRoutines.ModulesPublic;
@@ -24,149 +23,131 @@ public unsafe class AutoFaceCameraDirection : DailyModuleBase
     };
 
     public override ModulePermission Permission { get; } = new() { NeedAuth = true };
-    
-    public static bool  LockOn         { get; set; }
-    public static float LockOnRotation { get; set; }
-    
+
+    private const string COMMAND = "/pdrface";
+
     private static readonly CompSig                            CameraUpdateRotationSig = new("40 53 48 81 EC ?? ?? ?? ?? 8B 81 ?? ?? ?? ?? 48 8B D9 44 0F 29 54 24");
     private delegate        void                               CameraUpdateRotationDelegate(Camera* camera);
     private static          Hook<CameraUpdateRotationDelegate> CameraUpdateRotationHook;
-    
-    private static readonly Dictionary<string, Vector2> WorldDirectionToNormalizedDirection = new()
-    {
-        ["south"]     = new(0, 1),
-        ["north"]     = new(0, -1),
-        ["west"]      = new(-1, 0),
-        ["east"]      = new(1, 0),
-        ["northeast"] = new(0.707f, -0.707f),
-        ["southeast"] = new(0.707f, 0.707f),
-        ["northwest"] = new(-0.707f, -0.707f),
-        ["southwest"] = new(-0.707f, 0.707f),
-    };
-
-    private const string Command = "/pdrface";
 
     private static Config ModuleConfig = null!;
-    
+
     private static float LocalPlayerRotationInput;
 
     private static Camera* CacheCamera;
-    
+
+    private static bool  LockOn;
+    private static float LockOnRotation;
+
     protected override void Init()
     {
         ModuleConfig = LoadConfig<Config>() ?? new();
-        
+
         CameraUpdateRotationHook ??= CameraUpdateRotationSig.GetHook<CameraUpdateRotationDelegate>(CameraUpdateRotationDetour);
         CameraUpdateRotationHook.Enable();
-        
+
         UseActionManager.Instance().RegPostUseActionLocation(OnPostUseAction);
-        FrameworkManager.Instance().Reg(OnUpdate, throttleMS: 10);
 
-        CommandManager.AddCommand(Command, new(OnCommand) { HelpMessage = GetLoc("AutoFaceCameraDirection-CommandHelp", Command) });
-    }
-
-    protected override void ConfigUI()
-    {
-        ImGui.AlignTextToFramePadding();
-        ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), $"{GetLoc("WorkMode")}:");
-
-        ImGui.SameLine();
-        if (ImGuiComponents.ToggleButton("WorkMode", ref ModuleConfig.WorkMode))
-            SaveConfig(ModuleConfig);
-        
-        ImGui.SameLine();
-        ImGui.TextWrapped($"{GetLoc($"AutoFaceCameraDirection-WorkMode{ModuleConfig.WorkMode}")}");
-        
-        ImGui.Spacing();
-
-        ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), $"{GetLoc("Command")}:");
-        
-        ImGui.SameLine();
-        ImGui.TextUnformatted($"{Command} → {GetLoc("AutoFaceCameraDirection-CommandHelp", Command)}");
-        
-        ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), $"<{GetLoc("Type")}> :");
-        
-        using (ImRaii.PushIndent())
-        {
-            ImGui.TextUnformatted($"ground ({GetLoc("AutoFaceCameraDirection-GroundDirection")})");
-            
-            ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), $"<{GetLoc("Value")}> :");
-            
-            ImGui.SameLine();
-            ImGui.TextUnformatted($"{string.Join(" / ", WorldDirectionToNormalizedDirection.Keys)}");
-            
-            ImGui.TextUnformatted($"chara ({GetLoc("AutoFaceCameraDirection-CharacterRotation")})");
-            
-            ImGui.TextUnformatted($"camera ({GetLoc("AutoFaceCameraDirection-CameraRotation")})");
-        }
-        
-        if (DService.Instance().ObjectTable.LocalPlayer is not { } localPlayer) return;
-        using (ImRaii.Group())
-        {
-            ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), $"{GetLoc("AutoFaceCameraDirection-GroundDirection")}:");
-
-            using (ImRaii.PushIndent())
-            {
-                foreach (var kvp in WorldDirectionToNormalizedDirection)
-                {
-                    if (ImGui.Button($"{kvp.Key}##WorldDirectionToNormalizedDirection"))
-                        localPlayer.ToStruct()->SetRotation(WorldDirHToCharaRotation(kvp.Value));
-                    ImGui.SameLine();
-                }
-                ImGui.NewLine();
-            }
-
-            using (ImRaii.Group())
-            {
-                ImGui.AlignTextToFramePadding();
-                ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), $"{GetLoc("AutoFaceCameraDirection-CharacterRotation")}:");
-                
-                ImGui.SameLine();
-                ImGui.SetNextItemWidth(100f * GlobalFontScale);
-                ImGui.InputFloat("###CurrentCharaRotation", ref LocalPlayerRotationInput, 0, 0, "%.2f");
-                if (ImGui.IsItemDeactivatedAfterEdit())
-                    localPlayer.ToStruct()->SetRotation(LocalPlayerRotationInput);
-                
-                ImGui.SameLine();
-                ImGui.TextUnformatted($"{localPlayer.Rotation:F2}");
-            }
-            
-            ImGui.SameLine();
-            ImGui.TextDisabled("|");
-            
-            ImGui.SameLine();
-            using (ImRaii.Group())
-            {
-                var camera = (CameraEx*)CameraManager.Instance()->GetActiveCamera();
-                ImGui.AlignTextToFramePadding();
-                ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(),
-                                  $"{GetLoc("AutoFaceCameraDirection-CameraRotation")} → " +
-                                  $"{GetLoc("AutoFaceCameraDirection-CharacterRotation")}:");
-                
-                ImGui.SameLine();
-                ImGui.TextUnformatted($"{camera->DirH:F2} → {CameraDirHToCharaRotation(camera->DirH):F2}");
-            }
-        }
+        CommandManager.AddCommand(COMMAND, new(OnCommand) { HelpMessage = GetLoc("AutoFaceCameraDirection-CommandHelp", COMMAND) });
     }
 
     protected override void Uninit()
     {
-        FrameworkManager.Instance().Unreg(OnUpdate);
         UseActionManager.Instance().Unreg(OnPostUseAction);
-        
-        CommandManager.RemoveCommand(Command);
+
+        CommandManager.RemoveCommand(COMMAND);
+    }
+
+    protected override void ConfigUI()
+    {
+        ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), $"{GetLoc("Command")}");
+
+        using (ImRaii.PushIndent())
+        {
+            ImGui.TextUnformatted($"{COMMAND} → {GetLoc("AutoFaceCameraDirection-CommandHelp", COMMAND)}");
+
+            ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), $"<{GetLoc("Type")}>");
+
+            using (ImRaii.PushIndent())
+            {
+                ImGui.TextUnformatted($"ground ({GetLoc("AutoFaceCameraDirection-GroundDirection")})");
+
+                using (ImRaii.PushIndent())
+                    ImGui.TextUnformatted($"({GroundValuesString})");
+
+                ImGui.TextUnformatted($"chara ({GetLoc("AutoFaceCameraDirection-CharacterRotation")})");
+
+                ImGui.TextUnformatted($"camera ({GetLoc("AutoFaceCameraDirection-CameraRotation")})");
+            }
+        }
+
+        ImGui.NewLine();
+
+        ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), $"{GetLoc("WorkMode")}");
+
+        ImGui.SameLine();
+        if (ImGuiComponents.ToggleButton("WorkMode", ref ModuleConfig.WorkMode))
+            SaveConfig(ModuleConfig);
+
+        using (ImRaii.PushIndent())
+            ImGui.TextWrapped($"{GetLoc($"AutoFaceCameraDirection-WorkMode{ModuleConfig.WorkMode}")}");
+
+        if (DService.Instance().ObjectTable.LocalPlayer is not { } localPlayer) return;
+
+        ImGui.NewLine();
+
+        ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), $"{GetLoc("AutoFaceCameraDirection-GroundDirection")}");
+
+        using (ImRaii.PushIndent())
+        {
+            foreach (var kvp in WorldDirectionToNormalizedDirection)
+            {
+                if (ImGui.Button($"{kvp.Key}##WorldDirectionToNormalizedDirection"))
+                    localPlayer.ToStruct()->SetRotation(WorldDirHToCharaRotation(kvp.Value));
+                ImGui.SameLine();
+            }
+
+            ImGui.NewLine();
+        }
+
+        ImGui.TextColored(KnownColor.LightSkyBlue.ToVector4(), $"{GetLoc("AutoFaceCameraDirection-CharacterRotation")}");
+
+        using (ImRaii.ItemWidth(200f * GlobalFontScale))
+        using (ImRaii.PushIndent())
+        {
+            ImGui.InputFloat($"{GetLoc("Settings")}##SetCharaRotation", ref LocalPlayerRotationInput, format: "%.2f");
+            if (ImGui.IsItemDeactivatedAfterEdit())
+                localPlayer.ToStruct()->SetRotation(LocalPlayerRotationInput);
+
+            var currentRotation = localPlayer.Rotation;
+            ImGui.InputFloat($"{GetLoc("Current")}##CurrentCharaRotation", ref currentRotation, format: "%.2f", flags: ImGuiInputTextFlags.ReadOnly);
+        }
+
+        if (CacheCamera == null) return;
+
+        ImGui.TextColored
+        (
+            KnownColor.LightSkyBlue.ToVector4(),
+            $"{GetLoc("AutoFaceCameraDirection-CameraRotation")} → " +
+            $"{GetLoc("AutoFaceCameraDirection-CharacterRotation")}:"
+        );
+
+        using (ImRaii.PushIndent())
+            ImGui.TextUnformatted($"{CacheCamera->DirH:F2} → {CameraDirHToCharaRotation(CacheCamera->DirH):F2}");
     }
 
     private static void OnCommand(string command, string args)
     {
         args = args.Trim().ToLowerInvariant();
+
         if (string.IsNullOrWhiteSpace(args))
         {
             NotifyCommandError();
             return;
         }
-        
+
         var arguments = args.Split(' ');
+
         if (arguments.Length is not (1 or 2) || DService.Instance().ObjectTable.LocalPlayer is not { } localPlayer)
         {
             NotifyCommandError();
@@ -187,11 +168,11 @@ public unsafe class AutoFaceCameraDirection : DailyModuleBase
 
                 LockOnRotation = WorldDirHToCharaRotation(dirGround);
                 LockOn         = true;
-                
+
                 localPlayer.ToStruct()->SetRotation(LockOnRotation);
                 break;
             case "chara":
-                if (!float.TryParse(valueRaw, out var rotation)) 
+                if (!float.TryParse(valueRaw, out var rotation))
                 {
                     NotifyCommandError();
                     return;
@@ -206,10 +187,10 @@ public unsafe class AutoFaceCameraDirection : DailyModuleBase
                     NotifyCommandError();
                     return;
                 }
-                
+
                 LockOnRotation = CameraDirHToCharaRotation(dirCamera);
                 LockOn         = true;
-                
+
                 localPlayer.ToStruct()->SetRotation(LockOnRotation);
                 break;
             case "off":
@@ -220,67 +201,71 @@ public unsafe class AutoFaceCameraDirection : DailyModuleBase
                 NotifyCommandError();
                 return;
         }
-        
+
         if (!LockOn) return;
-        
+
         localPlayer.ToStruct()->SetRotation(LockOnRotation);
-        
-        if (BoundByDuty) 
+
+        if (BoundByDuty)
             PositionUpdateInstancePacket.Send(LockOnRotation, localPlayer.Position);
-        else 
+        else
             new PositionUpdatePacket(LockOnRotation, localPlayer.Position).Send();
-        
+
         return;
 
-        void NotifyCommandError() => 
+        void NotifyCommandError() =>
             NotificationError(GetLoc("Commands-InvalidArgs", command, args));
     }
-    
-    private static void OnPostUseAction(bool result, ActionType actionType, uint actionID, ulong targetID, Vector3 location, uint extraParam, byte a7)
+
+    private static void OnPostUseAction
+    (
+        bool       result,
+        ActionType actionType,
+        uint       actionID,
+        ulong      targetID,
+        Vector3    location,
+        uint       extraParam,
+        byte       a7
+    )
     {
         if (!result) return;
 
         Throttler.Remove("AutoFaceCameraDirection-UpdateRotationInstance");
         Throttler.Remove("AutoFaceCameraDirection-UpdateRotation");
-        
+
         if (CacheCamera != null)
             CameraUpdateRotationDetour(CacheCamera);
-    }
-    
-    private static void OnUpdate(IFramework framework)
-    {
-        if (CacheCamera == null) return;
-        CameraUpdateRotationDetour(CacheCamera);
     }
 
     private static void CameraUpdateRotationDetour(Camera* camera)
     {
         CameraUpdateRotationHook.Original(camera);
+        CacheCamera = camera;
 
         if (MovementManager.IsManagerBusy ||
-            BetweenAreas                  ||
             OccupiedInEvent               ||
             DService.Instance().ObjectTable.LocalPlayer is not { CurrentHp: > 0 } localPlayer)
             return;
-        
+
         switch (ModuleConfig.WorkMode)
         {
             case false when !IsConflictKeyPressed():
-            case true  when IsConflictKeyPressed():
-                
+            case true when IsConflictKeyPressed():
+
                 // 不能发包, 发了直接断读条
                 if (localPlayer.IsCasting) break;
 
-                var currentRotation = MathF.Round(localPlayer.Rotation, 2);
-                var targetRotation  = MathF.Round(LockOn ? LockOnRotation : CameraDirHToCharaRotation(((CameraEx*)camera)->DirH), 4);
-                
+                var currentRotation = MathF.Round(localPlayer.Rotation,                                              2);
+                var targetRotation  = MathF.Round(LockOn ? LockOnRotation : CameraDirHToCharaRotation(camera->DirH), 4);
+
                 var isRotationChanged = IsRotationChanged(currentRotation, targetRotation);
                 var currentMoveState  = MovementManager.CurrentZoneMoveState;
+
                 localPlayer.ToStruct()->SetRotation(targetRotation);
-                if (BoundByDuty)
+
+                if (GameState.ContentFinderCondition != 0)
                 {
                     if (!isRotationChanged && !DService.Instance().Condition[ConditionFlag.InCombat]) break;
-                    if (!Throttler.Throttle("AutoFaceCameraDirection-UpdateRotationInstance", 10)) break;
 
                     var moveType = (PositionUpdateInstancePacket.MoveType)(currentMoveState * 0x10000);
                     new PositionUpdateInstancePacket(targetRotation, localPlayer.Position, moveType).Send();
@@ -289,11 +274,11 @@ public unsafe class AutoFaceCameraDirection : DailyModuleBase
                 {
                     if (!isRotationChanged && !DService.Instance().Condition[ConditionFlag.InCombat]) break;
                     if (!Throttler.Throttle("AutoFaceCameraDirection-UpdateRotation", 20)) break;
-                    
+
                     var moveType = (PositionUpdatePacket.MoveType)(currentMoveState * 0x10000);
                     new PositionUpdatePacket(targetRotation, localPlayer.Position, moveType).Send();
                 }
-                
+
                 break;
         }
     }
@@ -303,15 +288,15 @@ public unsafe class AutoFaceCameraDirection : DailyModuleBase
         // true - 按下打断热键才让人物面向与摄像机一致
         // false - 按下打断热键则不保持人物面向与摄像机一致
         public bool WorkMode;
-    } 
-    
+    }
+
     [IPCProvider("DailyRoutines.Modules.AutoFaceCameraDirection.SetWorkMode")]
     public static void SetWorkModeIPC(bool workMode) => ModuleConfig.WorkMode = workMode;
 
     [IPCProvider("DailyRoutines.Modules.AutoFaceCameraDirection.CancelLockOn")]
     public static void CancelLockOnIPC()
     {
-        LockOn = false;
+        LockOn         = false;
         LockOnRotation = 0;
     }
 
@@ -322,7 +307,7 @@ public unsafe class AutoFaceCameraDirection : DailyModuleBase
             return false;
 
         LockOnRotation = WorldDirHToCharaRotation(dirGround);
-        LockOn = true;
+        LockOn         = true;
 
         return true;
     }
@@ -331,13 +316,27 @@ public unsafe class AutoFaceCameraDirection : DailyModuleBase
     public static void LockOnCharaIPC(float rotation)
     {
         LockOnRotation = rotation;
-        LockOn = true;
+        LockOn         = true;
     }
 
     [IPCProvider("DailyRoutines.Modules.AutoFaceCameraDirection.LockOnCamera")]
     public static void LockOnCameraIPC(float rotation)
     {
         LockOnRotation = CameraDirHToCharaRotation(rotation);
-        LockOn = true;
+        LockOn         = true;
     }
+
+    private static readonly FrozenDictionary<string, Vector2> WorldDirectionToNormalizedDirection = new Dictionary<string, Vector2>
+    {
+        ["south"]     = new(0, 1),
+        ["north"]     = new(0, -1),
+        ["west"]      = new(-1, 0),
+        ["east"]      = new(1, 0),
+        ["northeast"] = new(0.707f, -0.707f),
+        ["southeast"] = new(0.707f, 0.707f),
+        ["northwest"] = new(-0.707f, -0.707f),
+        ["southwest"] = new(-0.707f, 0.707f)
+    }.ToFrozenDictionary();
+
+    private static readonly string GroundValuesString = string.Join(" / ", WorldDirectionToNormalizedDirection.Keys);
 }
