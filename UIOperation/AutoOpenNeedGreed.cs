c38fe0ba60adac32bbc3982e5c53341fdebab64e
AtmoOmen <atmoomen2004@outlook.com>
2026-01-21 16:43:49 +0800

    AutoOpenNeedGreed

---
+++ UIOperation/AutoOpenNeedGreed.cs
@@ -0,0 +1,29 @@
+﻿using DailyRoutines.Abstracts;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+
+namespace DailyRoutines.ModulesPublic;
+
+public class AutoOpenNeedGreed : DailyModuleBase
+{
+    public override ModuleInfo Info { get; } = new()
+    {
+        Title           = GetLoc("AutoOpenNeedGreedTitle"),
+        Description     = GetLoc("AutoOpenNeedGreedDescription"),
+        Category        = ModuleCategories.UIOperation,
+    };
+
+    public override ModulePermission Permission { get; } = new() { AllDefaultEnabled = true };
+
+    protected override void Init() =>
+        LogMessageManager.Instance().RegPost(OnPost);
+    
+    protected override void Uninit() =>
+        LogMessageManager.Instance().Unreg(OnPost);
+
+    private static unsafe void OnPost(uint logMessageID, LogMessageQueueItem item)
+    {
+        if (logMessageID != 5194) return;
+
+        NeedGreed->Callback(0, 0U);
+    }
+}
