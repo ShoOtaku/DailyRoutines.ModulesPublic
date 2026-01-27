d6f1455b034acc3c367a1a88a9fa6159f6c87e31
AtmoOmen <atmoomen2004@outlook.com>
2026-01-27 11:30:08 +0800

    改一下

--- UIOperation/AutoOpenNeedGreed.cs
+++ UIOperation/AutoOpenNeedGreed.cs
@@ -1,5 +1,7 @@
 ﻿using DailyRoutines.Abstracts;
+using Dalamud.Game.ClientState.Objects.Enums;
 using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using AgentId = FFXIVClientStructs.FFXIV.Client.UI.Agent.AgentId;
 
 namespace DailyRoutines.ModulesPublic;
 
@@ -14,16 +16,28 @@ public class AutoOpenNeedGreed : DailyModuleBase
 
     public override ModulePermission Permission { get; } = new() { AllDefaultEnabled = true };
 
-    protected override void Init() =>
+    protected override void Init()
+    {
+        TaskHelper ??= new() { TimeoutMS = 3_000 };
         LogMessageManager.Instance().RegPost(OnPost);
-    
-    protected override void Uninit() =>
+        TargetManager.Instance().RegPostInteractWithObject(OnPostInteractWithObject);
+    }
+
+    protected override void Uninit()
+    {
         LogMessageManager.Instance().Unreg(OnPost);
+        TargetManager.Instance().Unreg(OnPostInteractWithObject);
+    }
+    
+    private static void OnPostInteractWithObject(ulong result, IGameObject? target, bool checkLoS)
+    {
+        if (result == 0 || target is not { ObjectKind: ObjectKind.Treasure }) return;
+        Throttler.Throttle("AutoOpenNeedGreed-SelfOpen", 1_000, true);
+    }
 
     private static unsafe void OnPost(uint logMessageID, LogMessageQueueItem item)
     {
-        if (logMessageID != 5194) return;
-
-        NeedGreed->Callback(0, 0U);
+        if (logMessageID != 5194 || !Throttler.Check("AutoOpenNeedGreed-SelfOpen")) return;
+        AgentId.Hud.SendEvent(0, 0, 2, " ");
     }
 }
